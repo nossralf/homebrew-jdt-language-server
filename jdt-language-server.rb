59858f476a77994c9a6ca994f246cb1a2aae35bf
Fredrik Larsson <nossralf@gmail.com>
2017-09-26 20:47:42 +0200

    New version

--- jdt-language-server.rb
+++ jdt-language-server.rb
@@ -2,9 +2,9 @@ class JdtLanguageServer < Formula
   desc "A Java language specific implementation of the Language Server Protocol"
   homepage "https://github.com/eclipse/eclipse.jdt.ls"
 
-  url "http://download.eclipse.org/jdtls/snapshots/jdt-language-server-0.5.0-201709251422.tar.gz"
-  sha256 "d8da7af322696033550ee66dbe497c9186b23c9d04d146d5769c0717cec01a3d"
-  version "0.5.0-201709251422"
+  url "http://download.eclipse.org/jdtls/snapshots/jdt-language-server-0.5.0-201709261535.tar.gz"
+  sha256 "d98fa8522d9db16c585a772c722f418b0a1420140ca930d391f2a7721e296dc7"
+  version "0.5.0-201709261535"
 
   bottle :unneeded
 
